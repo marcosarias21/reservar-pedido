9065a4258c51d9b57467a89989cca2eaa258400e
marcosarias21 <marcosariasolaiz@gmail.com>
2023-01-19 11:33:29 -0300

    los menus se pueden editar correctamente desde el admin

--- src/pages/MenusPage/MenusPage.js
+++ src/pages/MenusPage/MenusPage.js
@@ -1,17 +1,42 @@
-import React, { useContext } from 'react';
+import {
+  useContext, useEffect, useRef, useState,
+} from 'react';
 import { MenusAdmin } from '../../components/MenusAdmin';
 import { Nabvar } from '../../components/Navbar';
 import userContext from '../../provider/UserContext';
+import './menuspage.scss';
 
 const MenusPage = () => {
-  const { menuData, dataUser } = useContext(userContext);
-  console.log(menuData);
+  const { dataUser } = useContext(userContext);
+  const tableRef = useRef();
+  const [allMenu, setAllMenu] = useState([]);
+  const getAllMenus = async () => {
+    const resp = await fetch('http://localhost:8000/menu');
+    const json = await resp.json();
+    setAllMenu(json.menu);
+  };
+
+  useEffect(() => {
+    getAllMenus();
+  }, []);
+
   return (
     <>
     <Nabvar dataUser={dataUser.user} />
+    <table className='table' ref={tableRef}>
+        <thead>
+        <tr>
+              <th className='fw-bold ps-3 py-3'>ID</th>
+              <th className='fw-bold ps-3 py-3'>Menu</th>
+              <th className='fw-bold ps-3 py-3'>Empresa</th>
+              <th></th>
+            </tr>
+        </thead>
+
       {
-        menuData?.map(menu => <MenusAdmin key={menu.id} {...menu} />)
+        allMenu?.map(menu => <MenusAdmin key={menu.id} {...menu} tableRef={tableRef} />)
       }
+    </table>
     </>
   );
 };
