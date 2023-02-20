eb70c667c19a8591bd46b70bb91ed06e631cc108
marcosarias21 <marcosariasolaiz@gmail.com>
2023-02-20 12:58:41 -0300

    cambio de color del background del navbar y agregue iconos tanto para el admin como para el cliente

--- src/components/DropdownUser/DropdownUser.js
+++ src/components/DropdownUser/DropdownUser.js
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
+import { RxAvatar } from 'react-icons/rx';
+import { CiLogout } from 'react-icons/ci';
+import { AiOutlineHistory } from 'react-icons/ai';
 import useLogout from '../../hooks/useLogout';
 
 const DropdownUser = ({ user }) => {
   const logOut = useLogout();
   return (
     <div className="dropdown-center me-4">
-      <button className="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        {user.nombre} {user.apellido}
-      </button>
+      <button className="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false"><RxAvatar /> {user.nombre} {user.apellido}</button>
       <ul className="dropdown-menu">
-        <li><Link className="dropdown-item" to='/pedidos'>Mis pedidos</Link></li>
+        <li><Link className="dropdown-item" to='/pedidos'><AiOutlineHistory /> Mis pedidos</Link></li>
         <li><hr className="dropdown-divider"/></li>
-        <li><a className="dropdown-item" onClick={logOut}>Cerrar Sesion</a></li>
+        <li><a className="dropdown-item" onClick={logOut}><CiLogout />Cerrar Sesion</a></li>
       </ul>
     </div>
   );
