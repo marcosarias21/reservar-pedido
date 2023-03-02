4bd395bb103b952bcd1e0ed2d17191f4e2b4463e
marcosarias21 <marcosariasolaiz@gmail.com>
2023-03-02 11:04:45 -0300

    agregue algunos iconos en la seccion de clientes

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsClockHistory } from 'react-icons/bs';
 import { ListOrder } from '../ListOrder';
 
 const Clients = ({
@@ -16,10 +18,10 @@ const Clients = ({
       <div className="card col-3 mt-1">
         <div className="card-body">
           <h5 className="card-title">Cliente: {nombre} {apellido}</h5>
-          <p className="card-text">Mail: <span className="fw-bold">{email}</span></p>
+          <p className="card-text"><AiOutlineMail /> Email : <span className="fw-bold">{email}</span></p>
           <p className="card-text">Empresa: <span className="fw-bold">{empresa}</span></p>
           <hr/>
-          <button type="button" onClick={() => setUserPedido(!userPedido)} className="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Historial de pedidos:</button>
+          <button type="button" onClick={() => setUserPedido(!userPedido)} className="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><BsClockHistory /> Historial de pedidos:</button>
           {
             userPedido ? orderSortDate?.map(pedido => <ListOrder key={pedido.id} {...pedido} />)
               : null
--- src/components/ListOrder/ListOrder.js
+++ src/components/ListOrder/ListOrder.js
@@ -1,9 +1,11 @@
+import { BsFillCalendarDateFill } from 'react-icons/bs';
+import { BiFoodMenu } from 'react-icons/bi';
 import './listorder.scss';
 
 const ListOrder = ({ pedido, hora }) => {
   return (
     <ul className="list-group">
-      <li className="list-group-item"> Dia: {hora} - Pedido: {pedido}</li>
+      <li className="list-group-item"> <BsFillCalendarDateFill /> <span className='fw-bold'>Dia</span> : {hora} - <BiFoodMenu /> <span className='fw-bold'>Pedido</span>: {pedido}</li>
     </ul>
   );
 };
