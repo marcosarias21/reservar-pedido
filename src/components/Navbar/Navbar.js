f9f3f743838ea4942a8494c8f71d16db6adcab16
marcosarias21 <marcosariasolaiz@gmail.com>
2023-03-06 11:16:37 -0300

    mini detalle agregado en el navbar para que cuando estemos en la seccion del navlink se active

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { DropdownAdmin } from '../DropdownAdmin';
 import { DropdownUser } from '../DropdownUser';
 import gorritoIcono from '../../assets/images/cocinero.png';
@@ -10,7 +10,7 @@ const Navbar = ({ user, className }) => {
       <div className="container-fluid container">
         <div>
           <img src={gorritoIcono} className='size-icon' />
-          <NavLink className="navbar-brand fw-bold nav-title" to="/home">Reserva tu Pedido</NavLink>
+          <Link className="navbar-brand fw-bold nav-title" to="/home">Reserva tu Pedido</Link>
         </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -18,10 +18,12 @@ const Navbar = ({ user, className }) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mb-2 mb-lg-0 mx-auto">
             <li className="nav-item">
-              <NavLink className="nav-link active fw-bold" aria-current="page" to="/home">Home</NavLink>
+              <NavLink className={({ isActive }) => (isActive ? 'nav-link active fw-bold' : 'nav-link fw-bold')
+            } aria-current="page" to="/home">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link active fw-bold" aria-current="page" to="/menus">Menu</NavLink>
+              <NavLink className={({ isActive }) => (isActive ? 'nav-link active fw-bold' : 'nav-link fw-bold')
+            } aria-current="page" to="/menus">Menu</NavLink>
             </li>
           </ul>
           <div>
