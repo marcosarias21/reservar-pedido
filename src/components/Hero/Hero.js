30687c315fb2cc6ad03a60f00436b55051a3a111
marcosarias21 <marcosariasolaiz@gmail.com>
2023-04-07 10:39:24 -0300

    responsive login, register y un poco del hero (faltan retoques)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,8 +5,8 @@ import './hero.scss';
 const Hero = () => {
   return (
     <div className="row hero-section gx-0">
-      <div className="col-6 d-flex flex-column justify-content-center align-items-center px-5">
-        <div className='w-50'>
+      <div className="col-12 col-sm-6 col-md-6 d-flex flex-column justify-content-center align-items-center px-5">
+        <div className='width-box'>
           <div className="d-flex">
             <h1 className="text-color-title fw-bold">Reserva</h1>
             <div className="d-flex flex-column justify-content-center ms-3">
@@ -22,7 +22,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="col-6 d-flex justify-content-center align-items-center">
+      <div className="img-display col-md-6 d-flex justify-content-center align-items-center">
         <img className='img-hero' src={backgroundImg} />
       </div>
     </div>
